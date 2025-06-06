0d2aa72918507d2592555121922fb1b283ae6349
cedarLongballa <longbacedar@gmail.com>
2025-06-06 14:58:12 -0500

    redid the player scanning so it is in general bot commands, modifiable based on radius given and returns a coordinate of the player based on the bot

--- game/common/General Bot/Dumb_bot.py
+++ game/common/General Bot/Dumb_bot.py
@@ -4,10 +4,9 @@ from General_Bot_Commands import *
 from game.common.avatar import Avatar
 from game.controllers.master_controller import MasterController
 from game.common.map.game_board import GameBoard
-from game.utils.vector import Vector
 
 
-class Dumb_Bot:
+class DumbBot:
     def __init__(self):
         super().__init__()
         self.vision = 1
@@ -17,11 +16,13 @@ class Dumb_Bot:
         self.player_seen : bool = False
         self.boosted : bool = False
         self.avatar: Avatar = Avatar()
+        self.playerX: 0
+        self.playerY: 0
 
     def movement(self):
         while not self.master_controller.game_over and not self.player_seen:
-            moveNum = random.randint(1,4)
-            match moveNum:
+            movenum = random.randint(1,4)
+            match movenum:
                 case 1:
                     moveUp(self)
                 case 2:
@@ -31,64 +32,34 @@ class Dumb_Bot:
                 case 4:
                     moveLeft(self)
 
-    def playerSeenMovement(self):
+    def player_seen_movement(self):
         while not self.master_controller.game_over and self.player_seen:
-            bot_pos: Vector = Vector(self.avatar.position.x, self.avatar.position.y)
-            if self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,1)), Avatar):
-                moveDown(self)
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,-1)), Avatar):
-                moveUp(self)
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,0)), Avatar):
-                moveRight(self)
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,0)), Avatar):
-                moveLeft(self)
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,1)), Avatar):
-                moveNum = random.randint(1,2)
-                if moveNum == 1:
+            if self.playerX > 0 and self.playerY > 0:
+                if self.playerY > self.playerX:
                     moveDown(self)
-                elif moveNum == 2:
-                    moveRight(self)
                 else:
-                    self.movement()
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,1)), Avatar):
-                moveNum = random.randint(1,2)
-                if moveNum == 1:
+                    moveRight(self)
+            elif self.playerX < 0 and self.playerY < 0:
+                if self.playerY < self.playerX:
+                    moveUp(self)
+                else:
+                    moveLeft(self)
+            elif self.playerX > 0 and self.playerY < 0:
+                if abs(self.playerY) > self.playerX:
+                    moveUp(self)
+                else:
+                    moveRight(self)
+            elif self.playerX < 0 and self.playerY > 0:
+                if self.playerY > abs(self.playerX):
                     moveDown(self)
-                elif moveNum == 2:
+                else:
                     moveLeft(self)
-                else:
-                    self.movement()
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,-1)), Avatar):
-                moveNum = random.randint(1,2)
-                if moveNum == 1:
-                    moveUp(self)
-                elif moveNum == 2:
-                    moveRight(self)
-                else:
-                    self.movement()
-            elif self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,-1)), Avatar):
-                moveNum = random.randint(1,2)
-                if moveNum == 1:
-                    moveUp(self)
-                elif moveNum == 2:
-                    moveLeft(self)
-                else:
-                    self.movement()
             else:
                 self.movement()
 
-    def playerScan(self):
+    def player_scan(self):
         while not self.master_controller.game_over:
-            bot_pos: Vector = Vector(self.avatar.position.x, self.avatar.position.y)
             if not self.boosted:
-                if not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,-1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,-1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,-1)), Avatar) :
-                    self.player_seen = False
-                else:
-                    self.player_seen = True
+                self.player_seen, self.playerX, self.playerY = playerScan(self, 1)
             elif self.boosted:
-                if not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,-1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,-1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,-1)), Avatar)\
-                        and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-2,-2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1,-2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,-2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,-2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(2,-2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(2,-1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(2,0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(2,1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(2,2)), Avatar) \
-                        and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(1,2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(0,2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-1, 2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-2, 2)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-2, 1)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-2, 0)), Avatar) and not self.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-2, -1)), Avatar):
-                    self.player_seen = False
-                else:
-                    self.player_seen = True
+                self.player_seen, self.playerX, self.playerY = playerScan(self, 2)
--- game/common/General Bot/General_Bot_Commands.py
+++ game/common/General Bot/General_Bot_Commands.py
@@ -1,14 +1,46 @@
+from game.common.avatar import Avatar
 from game.common.enums import ActionType
 from game.common.player import Player
 from game.controllers.movement_controller import MovementController
 from game.common.map.game_board import GameBoard
+from game.utils.vector import Vector
 
 player = Player()
+
 def moveLeft(bot):
     MovementController.handle_actions(bot, ActionType(4), player, GameBoard)
+
 def moveUp(bot):
     MovementController.handle_actions(bot, ActionType(2), player, GameBoard)
+
 def moveRight(bot):
     MovementController.handle_actions(bot, ActionType(5), player, GameBoard)
+
 def moveDown(bot):
     MovementController.handle_actions(bot, ActionType(3), player, GameBoard)
+
+def playerScan(bot, radius):
+    bot_pos: Vector = Vector(bot.avatar.position.x, bot.avatar.position.y)
+    i = 1
+    while(i <= radius):
+        j = 0
+        while(j <= radius):
+            if (not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(j, i)),Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(j, -i)), Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(i, j)), Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-i, j)), Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-i, -j)), Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(i, -j)), Avatar) and not bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-j, i)), Avatar)):
+                j += 1
+            else:
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(j, i)),Avatar):
+                    return True, j, i
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(j, -i)),Avatar):
+                    return True, j, -i
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(i, j)),Avatar):
+                    return True, i, j
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-i, j)),Avatar):
+                    return True, -i, j
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-i, -j)),Avatar):
+                    return True, -i, -j
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(i, -j)),Avatar):
+                    return True, i, -j
+                if bot.game_board.get_objects_from(Vector(bot_pos.add_to_vector(-j, i)),Avatar):
+                    return True, -j, i
+        i += 1
+    return False, 0, 0
