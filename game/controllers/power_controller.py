bfcd1938fa0a0a4312451f80645ccc7f2ad86157
Noah M <noah.magcalas@gmail.com>
2025-06-10 20:08:41 -0500

    feat: power drain

---
+++ game/controllers/power_controller.py
@@ -0,0 +1,28 @@
+from game.common.enums import ActionType
+from game.common.map.game_board import GameBoard
+from game.common.player import Player
+from game.controllers.controller import Controller
+
+
+class PowerController(Controller):
+    """
+    manages the passive drain of power from the player
+    """
+    def __init__(self, decay_frequency: int = 5, decay_amount = 0):
+        super().__init__()
+        self.__decay_frequency = decay_frequency # in turns
+        self.__decay_amount = decay_amount
+        self.__decay_tick = 0
+
+    def handle_actions(self, action: ActionType, client: Player, world: GameBoard):
+        del action # unused params
+
+        # only do stuff every decay_frequency turns
+        self.__decay_tick += 1
+        if self.__decay_tick < self.__decay_frequency:
+            return
+        self.__decay_tick = 0
+
+        client.avatar.power -= self.__decay_amount
+        # access generators from world
+        # generators should drain additional power
