df9832de7f072d961b448ccb735201eae80d0464
Noah Magcalas <noah.magcalas@gmail.com>
2025-07-05 15:26:33 -0500

    test: add unit tests for Generator

--- game/fnaacm/stations/generator.py
+++ game/fnaacm/stations/generator.py
@@ -1,4 +1,5 @@
 
+from typing import Self, override
 from game.common.avatar import Avatar
 from game.common.enums import ObjectType
 from game.common.items.item import Item
@@ -13,11 +14,28 @@ class Generator(Station):
     def __init__(self, held_item: Item | None = None, cost: int = 1):
         super().__init__(held_item=held_item)
         self.object_type: ObjectType = ObjectType.GENERATOR
-        self.cost = cost
-        self.__scrap = Scrap(quantity=self.cost)
+        self.cost: int = cost
+        # self.__scrap: Scrap = Scrap(quantity=self.cost)
 
     def take_action(self, avatar: Avatar) -> Item | None:
-        total_scrap = sum(map(lambda item: isinstance(item, Scrap), avatar.inventory))
+        # total_scrap = sum(map(lambda item: item.quantity if isinstance(item, Scrap) else 0, avatar.inventory))
+        total_scrap = 0
+        for item in avatar.inventory:
+            if not isinstance(item, Scrap):
+                continue
+            total_scrap += item.quantity
         if total_scrap < self.cost:
             return
-        avatar.take(self.__scrap)
+        avatar.take(Scrap(quantity=self.cost))
+
+    @override
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.cost = data['cost']
+        return self
+
+    @override
+    def to_json(self) -> dict:
+        jason = super().to_json()
+        jason['cost'] = self.cost
+        return jason
---
+++ game/test_suite/tests/test_generator.py
@@ -0,0 +1,49 @@
+
+import unittest
+
+from game.common.items.item import Item
+from game.common.avatar import Avatar
+from game.common.player import Player
+from game.common.map.game_board import GameBoard
+from game.controllers.interact_controller import InteractController
+from game.fnaacm.items.scrap import Scrap
+from game.utils.vector import Vector
+from game.common.enums import ActionType
+from game.fnaacm.stations.generator import Generator
+
+class TestGenerator(unittest.TestCase):
+    def setUp(self) -> None:
+
+        position = Vector()
+        self.initial_scrap = 10
+        self.avatar = Avatar(position=position, max_inventory_size=self.initial_scrap)
+        self.scrap = Scrap(quantity=self.initial_scrap)
+        self.inventory: list[Item] = [self.scrap, ]
+        self.avatar.inventory = self.inventory
+
+        self.player = Player(avatar=self.avatar)
+
+        self.cost: int = 3
+        self.generator = Generator(cost=self.cost)
+        self.game_board = GameBoard(None, Vector(4, 4), { position: [self.generator, ], }, False)
+        self.game_board.generate_map()
+        self.interact_controller = InteractController()
+
+    # can the avatar interact with the generator?
+    def test_take_action(self):
+        self.assertGreater(self.initial_scrap, self.cost)
+        self.interact_controller.handle_actions(ActionType.INTERACT_CENTER, self.player, self.game_board)
+        self.assertEqual(self.scrap.quantity, self.initial_scrap - self.cost)
+
+    # what if they don't have enough scrap?
+    def test_not_enough_scrap(self):
+        initial_scrap = self.cost - 1
+        self.scrap.quantity = initial_scrap
+        self.interact_controller.handle_actions(ActionType.INTERACT_CENTER, self.player, self.game_board)
+        self.assertEqual(self.scrap.quantity, initial_scrap)
+
+    def test_json(self):
+        data: dict = self.generator.to_json()
+        generator: Generator = Generator().from_json(data)
+        self.assertEqual(self.generator.object_type, generator.object_type)
+        self.assertEqual(self.generator.cost, generator.cost)
