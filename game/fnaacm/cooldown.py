ef32c65944c610a0b96f7234dee0f8fa6adacf91
Noah Magcalas <noah.magcalas@gmail.com>
2025-07-13 20:13:15 -0500

    refactor: use `Cooldown` object for `Battery` cooldown stuff
    also implement __eq__ and to/from json methods for `Battery` and `Cooldown`

--- game/fnaacm/cooldown.py
+++ game/fnaacm/cooldown.py
@@ -1,9 +1,6 @@
 from typing import Self, override
 from game.common.game_object import GameObject
 
-DURATION_JSON_KEY = 'duration'
-COUNTER_JSON_KEY = 'counter'
-
 # TODO: testing 8^)
 
 class Cooldown(GameObject):
@@ -15,18 +12,23 @@ class Cooldown(GameObject):
         self.__duration = duration
         self.__counter = 0
 
+    def __eq__(self, value: object, /) -> bool:
+        if isinstance(value, self.__class__):
+            return self.__dict__ == value.__dict__
+        return False
+
     @override
     def to_json(self) -> dict:
         data = super().to_json()
-        data[DURATION_JSON_KEY] = self.__duration
-        data[COUNTER_JSON_KEY] = self.__counter
+        data['duration'] = self.__duration
+        data['counter'] = self.__counter
         return data
 
     @override
     def from_json(self, data: dict) -> Self:
         super().from_json(data)
-        self.__duration = data[DURATION_JSON_KEY]
-        self.__counter = data[COUNTER_JSON_KEY]
+        self.__duration = data['duration']
+        self.__counter = data['counter']
         return self
 
     def tick(self):
--- game/fnaacm/stations/battery.py
+++ game/fnaacm/stations/battery.py
@@ -1,34 +1,53 @@
+from typing import Self, override
 from game.common.avatar import Avatar
 from game.common.enums import ObjectType
 from game.common.map.occupiable import Occupiable
+from game.fnaacm.cooldown import Cooldown
 from game.utils.vector import Vector
-
 
 class Battery(Occupiable):
     """
     A tile that occasionally holds batteries which increase the player avatar's power.
     """
-    def __init__(self, position: Vector = Vector(0,0), cooldown: int = 10, recharge_amount: int = 10) -> None:
+    def __init__(self, position: Vector = Vector(0,0), cooldown_duration: int = 10, recharge_amount: int = 10) -> None:
         super().__init__()
         self.object_type: ObjectType = ObjectType.BATTERY
         self.position: Vector = position
         self.__recharge_amount: int = recharge_amount
-        self.__cooldown_length: int = cooldown
-        self.__cooldown_timer: int = 0
+        self.__cooldown = Cooldown(duration=cooldown_duration)
 
-    # should be called on every turn BEFORE any avatars interact with it
-    def tick(self) -> None:
-        # dont really gaf if it goes negative
-        self.__cooldown_timer -= 1
+    def __eq__(self, value: object, /) -> bool:
+        if isinstance(value, self.__class__):
+            return \
+                self.position == value.position and \
+                self.__recharge_amount == value.__recharge_amount and \
+                self.__cooldown == value.__cooldown 
+        return False
 
-    def handle_turn(self, avatar: Avatar) -> None:
-        if self.position != avatar.position:
-            return
-        if not self.is_available:
-            return
-        avatar.power += self.__recharge_amount
-        self.__cooldown_timer = self.__cooldown_length
+    @override
+    def to_json(self) -> dict:
+        data = super().to_json()
+        data['recharge_amount'] = self.__recharge_amount
+        data['cooldown'] = self.__cooldown.to_json()
+        data['position'] = self.position.to_json()
+        return data
+
+    @override
+    def from_json(self, data: dict) -> Self:
+        super().from_json(data)
+        self.__recharge_amount = data['recharge_amount']
+        self.__cooldown = Cooldown().from_json(data['cooldown'])
+        self.position = Vector().from_json(data['position'])
+        return self
 
     @property
     def is_available(self) -> bool:
-        return self.__cooldown_timer <= 0
+        return self.__cooldown.can_activate
+
+    def handle_turn(self, avatar: Avatar) -> None:
+        self.__cooldown.tick()
+        if self.position != avatar.position:
+            return
+        if not self.__cooldown.activate():
+            return
+        avatar.power += self.__recharge_amount
