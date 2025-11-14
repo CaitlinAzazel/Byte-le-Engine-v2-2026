e70930eab1154cde4a1bea2de7eb1fc8448d1998
cedarLongballa <longbacedar@gmail.com>
2025-11-14 17:24:56 -0600

    added stun methods to the support bot

--- game/fnaacm/bots/support_bot.py
+++ game/fnaacm/bots/support_bot.py
@@ -7,9 +7,27 @@ class SupportBot(Bot):
         self.turnedOn = False
         self.movement_controller: MovementController = MovementController()
         self.stun = False
+        self.stun_counter = 0
 
     def turned_on(self):
         return self.turnedOn
 
     def flip_state(self):
         self.turnedOn = not self.turnedOn
+
+    def player_hit_stun(self):
+        self.stun_counter += 15
+        self.stun = True
+        self.turnedOn = False
+
+    def scrap_stun(self):
+        self.stun_count += 5
+        self.stun = True
+        self.turnedOn = False
+
+    def action(self):
+        if self.stun:
+            self.stun_counter -= 1
+            if self.stun_counter == 0:
+                self.stun = False
+                self.turnedOn = True
