31626fddb36c6b1684d9509d52dcea2f37985fa8
ihoponyou <noah.magcalas@gmail.com>
2026-01-16 18:02:09 -0600

    fix: use Sphinx instead of mkdocs in the Sphinx action
    duh

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Byte-le Royale 2026 1.0.0'
+copyright = '1974-2026, NDSU ACM'
+author = 'NDSU ACM Byte-le 2026 Dev Team'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'furo'
+html_static_path = ['_static']
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+===============================================
+welcome toe tthe docs !
+===============================================
+
+these area the edocs
+
+.. .. image:: ./_static/images/cover_art.png
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   getting_started
+
+Search Page
+===========
+
+:ref:`search`
