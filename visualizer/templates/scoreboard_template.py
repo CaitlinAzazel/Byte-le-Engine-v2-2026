780120dc15e98255e8ebb686b1eb186837c992e0
ihoponyou <noah.magcalas@gmail.com>
2026-02-01 20:55:54 -0600

    feat: visualizer ui for health & power

--- visualizer/templates/scoreboard_template.py
+++ visualizer/templates/scoreboard_template.py
@@ -43,10 +43,30 @@ class ScoreboardTemplate(InfoTemplate):
             position=Vector(topleft.x + 600, topleft.y)
         )
 
+        self.health_text: Text = Text(
+            screen,
+            text="HP: 0",
+            font_size=36,
+            font_name=font,
+            color=color,
+            position=Vector(topleft.x + 800, topleft.y)
+        )
+
+        self.power_text: Text = Text(
+            screen,
+            text="Power:   0%",
+            font_size=36,
+            font_name=font,
+            color=color,
+            position=Vector(topleft.x + 1000, topleft.y)
+        )
+
         # Store current values for updates
         self.current_scrap = 0
         self.current_score = 0
         self.current_turn = 0
+        self.current_health = 0
+        self.current_power = 0
 
     def recalc_animation(self, turn_log: dict) -> None:
         """
@@ -59,15 +79,16 @@ class ScoreboardTemplate(InfoTemplate):
         # Update score
         if 'clients' in turn_log:
             # Sum all client scores and total scrap
-            self.current_score = sum(
-                client['avatar']['score'] if client.get('avatar') else 0
-                for client in turn_log['clients']
-            )
+            def sum_avatar_attr(attr: str):
+                return sum(
+                    client['avatar'][attr] if client.get('avatar') else 0
+                    for client in turn_log['clients']
+                )
 
-            self.current_scrap = sum(
-                client['avatar']['scrap'] if client.get('avatar') else 0
-                for client in turn_log['clients']
-            )
+            self.current_score = sum_avatar_attr('score')
+            self.current_scrap = sum_avatar_attr('scrap')
+            self.current_health = sum_avatar_attr('health')
+            self.current_power = sum_avatar_attr('power')
         else:
             self.current_score = turn_log.get('score', 0)
 
@@ -78,6 +99,8 @@ class ScoreboardTemplate(InfoTemplate):
         self.score.text = f"Score: {self.current_score}"
         self.turn.text = f"{self.current_turn} / {MAX_TICKS}"
         self.scrap.text = f"Scrap: {self.current_scrap}"
+        self.health_text.text = f"HP: {self.current_health}"
+        self.power_text.text = f"Power: {self.current_power:3}%"
 
     def render(self) -> None:
         """
@@ -86,3 +109,5 @@ class ScoreboardTemplate(InfoTemplate):
         self.score.render()
         self.turn.render()
         self.scrap.render()
+        self.health_text.render()
+        self.power_text.render()
