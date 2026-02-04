b44e725febcf23ba588ebe21e9cb5e050cc084e1
Noah Magcalas <noah.magcalas@gmail.com>
2026-02-03 21:28:44 -0600

    ci: use client build script
    :hurtrealbad:

--- client-build.sh
+++ client-build.sh
@@ -2,17 +2,16 @@
 
 shopt -s extglob
 
-test -e launcher.pyz && rm launcher.pyz
+mkdir output
+
 python compile_map_data.py
+echo "Building launcher..."
 cp -r game wrapper/game/
 cp -r visualizer wrapper/visualizer/
 mkdir -p wrapper/server
 cp -r server/!(*_temp|logs) wrapper/server/
-python -m zipapp wrapper -o launcher.pyz -c
-rm -r wrapper/game
-rm -r wrapper/visualizer
-rm -r wrapper/server
+python -m zipapp wrapper -o output/launcher.pyz -c
+echo "Copying extra files..."
+cp -r client_package/* output/
+
 echo "Build successful."
-echo "Copying to client package..."
-cp launcher.pyz ../client-package/launcher.pyz
-echo "Copy successful."
