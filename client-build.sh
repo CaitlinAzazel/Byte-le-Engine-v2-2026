c58cc2610740c1664594b3e78cfcd14872932935
Noah Magcalas <noah.magcalas@gmail.com>
2026-02-03 21:37:45 -0600

    ci: :rage2:

--- client-build.sh
+++ client-build.sh
@@ -4,13 +4,16 @@ shopt -s extglob
 
 mkdir output
 
+echo "Compiling map data..."
 python compile_map_data.py
+
 echo "Building launcher..."
 cp -r game wrapper/game/
 cp -r visualizer wrapper/visualizer/
 mkdir -p wrapper/server
 cp -r server/!(*_temp|logs) wrapper/server/
 python -m zipapp wrapper -o output/launcher.pyz -c
+
 echo "Copying extra files..."
 cp -r client_package/* output/
 
